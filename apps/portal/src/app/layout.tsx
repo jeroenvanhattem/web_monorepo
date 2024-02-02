83cd8bca2aa043207a3de2249bf1f2b5154ce46c
Jeroen <jvanhattem@onair.events>
2024-02-02 19:05:49 +0100

    Don't import store

--- apps/portal/src/app/layout.tsx
+++ apps/portal/src/app/layout.tsx
@@ -4,27 +4,27 @@ import "ui/style/main.scss";
 import { Open_Sans } from "next/font/google";
 import { useEffect } from "react";
 import { getReduxStateFromLocalstorage } from "redux-store/actions";
-import { store } from "redux-store/store";
+// import { store } from "redux-store/store";
 import { authSlice } from "redux-store/reducers";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.JSX.Element;
+	children: React.JSX.Element;
 }) {
-  useEffect(() => {
-    const reduxState = getReduxStateFromLocalstorage();
-    if (reduxState) {
-      store.dispatch(authSlice.actions.hydrate(reduxState.auth));
-    }
-  }, []);
-  return (
-    <html lang="en">
-      <Providers>
-        <body className={openSans.className}>{children}</body>
-      </Providers>
-    </html>
-  );
+	useEffect(() => {
+		const reduxState = getReduxStateFromLocalstorage();
+		if (reduxState) {
+			// store.dispatch(authSlice.actions.hydrate(reduxState.auth));
+		}
+	}, []);
+	return (
+		<html lang="en">
+			<Providers>
+				<body className={openSans.className}>{children}</body>
+			</Providers>
+		</html>
+	);
 }
--- apps/portal/src/utils/providers.tsx
+++ apps/portal/src/utils/providers.tsx
@@ -2,8 +2,12 @@
 
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from "redux-store/store";
+// import { store } from "redux-store/store";
 
 export const Providers = ({ children }: { children: React.JSX.Element }) => {
-  return <Provider store={store}>{children}</Provider>;
+	return (
+		// <Provider store={store}>
+		children
+		//  </Provider>
+	);
 };
